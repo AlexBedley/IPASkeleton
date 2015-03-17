442e0e2bf0e4322c038259427a40c695d022d371
Alex Bedley <alex.bedley@D2L.com>
2015-03-17 12:55:18 -0400

    added publish functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 'use strict';
 
 var frau = require('free-range-app-utils'),
-	gulp = require('gulp');
+	gulp = require('gulp'),
+	pjson = require('./package.json'),
+	publisher = require('gulp-frau-publisher'),
+	key = require('./private-key.js');
+
+var options = {
+    id: pjson.appId,
+    creds: {
+        "key": "AKIAJPKHVT3XFBAKFZWA",
+        "secret": key
+    },
+    devTag: 'test01'
+};
 
 var appFilename = 'app.js';
 var localAppResolver = frau.localAppResolver();
+var appPublisher = publisher.app( options );
 
 function makeAppConfig( target ) {
 	return frau.appConfigBuilder.buildStream( target + appFilename )
@@ -15,6 +28,15 @@ gulp.task( 'appconfig-local', function() {
 	return makeAppConfig( localAppResolver.getUrl() );
 } );
 
+gulp.task('appconfig-release', function () {
+    return makeAppConfig( appPublisher.getLocation() );
+});
+
 gulp.task( 'appresolver', function() {
 	localAppResolver.host();
 } );
+
+gulp.task( 'publish-release', function( cb ) {
+	gulp.src('./dist/**')
+		.pipe( appPublisher.getStream() );
+});
