51e7326965289f2597848ed941f2a2576fdfd545
Alex Bedley <alex.bedley@D2L.com>
2015-03-19 10:06:13 -0400

    add api and tests with some formatting

--- test/example/simple.tests.js
+++ test/example/simple.tests.js
@@ -1,11 +1,36 @@
+var request = require('superagent');
 var app = require('../../src/app.js');
 
 describe("A test suite", function() {
-    beforeEach(function() {});
-    afterEach(function() {});
+    before(function(done) {
+        sinon.stub(request, 'get', function(url) {
+            var resp = {
+                body: {
+                    FirstName: "Alex",
+                    LastName: "Bedley"
+                },
+                ok: true
+            };
+            var stub = {
+                use: function(auth) {
+                    return {
+                        end: function(cb) {
+                            cb(false, resp);
+                        }
+                    };
+                }
+            };
+            return stub;
+        });
+        done();
+    });
+    after(function(done) {
+        request.get.restore();
+        done();
+    });
     it('should pass', function() {
         var parent = {};
         app(parent);
-        expect(parent.innerHTML).to.be.equal("Hello, World!");
+        expect(parent.innerHTML).to.be.equal('Hello, Alex Bedley!');
     });
 });
